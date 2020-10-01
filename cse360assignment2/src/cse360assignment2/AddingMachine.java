b745f82cad4d49c7535552f475ff5fa594841743
thefakemacaw <38474041+thefakemacaw@users.noreply.github.com>
2020-10-01 11:56:41 -0700

    added comments and AddingMachine Javadoc

--- cse360assignment2/src/cse360assignment2/AddingMachine.java
+++ cse360assignment2/src/cse360assignment2/AddingMachine.java
@@ -1,23 +1,46 @@
 package cse360assignment2;
 
 public class AddingMachine {
+	
+	/*
+	 * A simple adding machine class that can add and subtract a variable
+	 */
+	
 	private int total;
 	private String history;
 
+		/**
+		 * Constructor for the AddingMachine Class
+		 */
 		public AddingMachine () {
 			total = 0;  // not needed - included for clarity
 			history = "0";
 		}
 
+		/**
+		 * Getter method that returns total
+		 * 
+		 * @return		total
+		 */
 		public int getTotal () {
 			return total;
 		}
 		
+		/**
+		 * Adds value parameter to total and updates the history string
+		 * 
+		 * @param	value	the number to be added to total
+		 */
 		public void add (int value) {
 			total += value;
 			history = history + " + " + value;
 		}
 
+		/**
+		 * Subtracts value parameter from total and updates the history string
+		 * 
+		 * @param	value	the number to be subtracted from total
+		 */
 		public void subtract (int value) {
 			total -= value;
 			history = history + " - " + value;
@@ -27,6 +50,9 @@ public class AddingMachine {
 			return history;
 		}
 
+		/**
+		 * Empty for now
+		 */
 		public void clear() {
 			
 		}
